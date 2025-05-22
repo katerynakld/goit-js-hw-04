d59b427e3fdd0bbf9555630e15505dfb64f00a9b
Kateryna Koliada <6parkseolhwa6@gmail.com>
2025-05-22 15:54:04 +0200

    Initial commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,24 @@
-function filterArray(numbers, value) {
-  const newArray = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (let i = 0; i < numbers.length; i++) {
-    const element = numbers[i];
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-    if (element > value) {
-      newArray.push(element);
-    }
-  }
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-  return newArray;
-}
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
